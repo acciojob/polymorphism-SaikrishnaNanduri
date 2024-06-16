d5edf486a48697d717723e4cdec73fe92a6f3ec8
root <root@next-hyd-9803.nexteducation.com>
2024-06-16 17:14:45 +0530

    Polymorphism assesment is done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+	public static class Product {
+		public int product(int x,int y) {
+			return x * y;
+		}
+		public int product(int x,int y,int z) {
+			return x * y * z;
+		}
+		public double product(double x,double y) {
+			return x * y;
+		}
+	}
+	public static void main(String[] args) {
+	 Product p = new Product();
+	 int p1= p.product(2, 3);
+	 int p2= p.product(2, 3, 4);
+	 double p3= p.product(2, 4);
+	 System.out.println("Product of 2 and 3: " + p1);
+     System.out.println("Product of 2, 3, and 4: " + p2);
+     System.out.println("Product of 2.5 and 3.5: " + p3);
+	 
+	}
 
 }
